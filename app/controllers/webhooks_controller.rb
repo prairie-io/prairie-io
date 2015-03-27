9f2b02b5c40badf2febd1f0e6a3e3297acb8bdbb
Matthew Sullivan <msull92@gmail.com>
2015-03-26 19:51:17 -0500

    Add extra if statement so mailchimp doesn't get pissed off.

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,9 +1,11 @@
 class WebhooksController < ApplicationController
   def mailchimp
-    user = User.find_by(email: params[:data][:email])
+    if params[:data] && params[:data][:email]
+      user = User.find_by(email: params[:data][:email])
 
-    if user
-      user.update_column(is_subscribed: false)
+      if user
+        user.update_column(is_subscribed: false)
+      end
     end
 
     render nothing: true
